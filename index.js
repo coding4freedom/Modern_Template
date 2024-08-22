ccedd53921c4fcff98f49368b7fae591aeefd570
coding4freedom <chawkins2010@hotmail.com>
2024-08-22 00:29:13 -0500

    updates to Media queries, site responsive complete

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
 
+document.addEventListener('DOMContentLoaded', () => {
+    const toggleBtn = document.querySelector('.nav__toggle');
+    const popupMenu = document.querySelector('.nav__popup');
+    const closeBtn = document.querySelector('.nav__close');
+
+    toggleBtn.addEventListener('click', () => {
+        popupMenu.style.display = 'flex';
+    });
+
+    closeBtn.addEventListener('click', () => {
+        popupMenu.style.display = 'none';
+    });
+
+    document.addEventListener('click', (e) => {
+        if (!popupMenu.contains(e.target) && !toggleBtn.contains(e.target)) {
+            popupMenu.style.display = 'none'
+        }
+    });
+});
